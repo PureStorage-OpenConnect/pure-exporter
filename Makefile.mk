09e3d6e14fd5a0429383a820ab85152a6afc0352
Eugenio Grosso <eugenio.grosso@purestorage.com>
2020-12-29 20:01:01 +0100

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -1,11 +1,13 @@
 IMAGE_NAMESPACE ?= quay.io/purestorage
 IMAGE_NAME ?= pure-exporter
 IMAGE_TAG ?= latest
+EXPORTER ?= pure_exporter
 REQUIREMENTS ?= requirements.txt
 DOCKERFILE ?= Dockerfile
 RUN_PORT ?= 9491
 TEST_PORT ?= 8123
 TIMEO ?= 30
+WORKERS ?= 2
 
 default: build
 
@@ -14,18 +16,18 @@ all: build test
 
 .PHONY: build
 build: $(DOCKERFILE) $(REQUIREMENTS) .dockerignore $(wildcard *.py)
-	docker build . -f $(DOCKERFILE) -t $(IMAGE_NAMESPACE)/$(IMAGE_NAME):$(IMAGE_TAG)
+        docker build . -f $(DOCKERFILE) -t $(IMAGE_NAMESPACE)/$(IMAGE_NAME):$(IMAGE_TAG)
 
 .PHONY: test-fa
 test:
-	(GUNICORN_CMD_ARGS="--bind=0.0.0.0:$(TEST_PORT) --workers=2 --access-logfile=- \
-         --timeout $(TIMEO) \
+        (GUNICORN_CMD_ARGS="--bind=0.0.0.0:$(TEST_PORT) --workers=2 --access-logfile=- \
+         --timeout $(TIMEO) --workers $(WORKERS) \
          --error-logfile=- --access-logformat=\"%(t)s %(h)s %(U)s %(l)s %(T)s %(B)s\"" \
-         gunicorn pure_fa_exporter:app)
+         gunicorn $(EXPORTER):app)
 
 .PHONY: test-fa-docker
 test-docker:
-	(GUNICORN_CMD_ARGS="--bind=0.0.0.0:$(RUN_PORT) --workers=2 --access-logfile=- \
-         --timeout $(TIMEO) \
+        (GUNICORN_CMD_ARGS="--bind=0.0.0.0:$(RUN_PORT) --workers=2 --access-logfile=- \
+         --timeout $(TIMEO) --workers $(WORKERS) \
          --error-logfile=- --access-logformat=\"%(t)s %(h)s %(U)s %(l)s %(T)s %(B)s\"" \
         docker run --rm -p $(TEST_PORT):$(RUN_PORT) $(IMAGE_NAMESPACE)/$(IMAGE_NAME):$(IMAGE_TAG))
